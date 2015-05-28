082bf93543f9e86e9da2f6830f9602e56bbe5660
Alex Ventura <alex.rv11@gmail.com>
2015-05-28 14:25:46 -0400

    fixed unit test it was broken in windows operating system

--- src/main/groovy/org/fundacionjala/gradle/plugins/enforce/utils/salesforce/component/validators/SalesforceValidatorManager.groovy
+++ src/main/groovy/org/fundacionjala/gradle/plugins/enforce/utils/salesforce/component/validators/SalesforceValidatorManager.groovy
@@ -1,10 +1,17 @@
 package org.fundacionjala.gradle.plugins.enforce.utils.salesforce.component.validators
-
+/**
+ * Manages the different kind of validate a file
+ * based in Salesforce definitions
+ */
 public class SalesforceValidatorManager {
     private static Map<String, SalesforceValidator> validatorMap = [
             'documents': new DocumentSalesforceValidator()
     ]
 
+    /**
+     * Returns a object validator based in the folderName
+     * defined in Salesforce
+     */
     public static SalesforceValidator getValidator(String folderName) {
         if (validatorMap.containsKey(folderName)) {
            return validatorMap.get(folderName)
