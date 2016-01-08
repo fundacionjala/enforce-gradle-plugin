86779dc9351c7cbbf4358e209ff7b57d319dbbc6
Dan Canqui <dan.canqui@jalasoft.com>
2016-01-08 10:43:10 -0400

    Add tests for validate encrypt/decrypt credential

---
+++ src/main/groovy/org/fundacionjala/gradle/plugins/enforce/tasks/credentialmanager/CredentialValidator.groovy
@@ -0,0 +1,18 @@
+/*
+ * Copyright (c) Fundacion Jala. All rights reserved.
+ * Licensed under the MIT license. See LICENSE file in the project root for full license information.
+ */
+
+package org.fundacionjala.gradle.plugins.enforce.tasks.credentialmanager
+
+import org.fundacionjala.gradle.plugins.enforce.wsc.Connector
+import org.fundacionjala.gradle.plugins.enforce.wsc.Credential
+
+class CredentialValidator {
+
+    public void validateCredential(Credential credential) throws Exception {
+    }
+
+    void validateCredential(Credential credential, Connector connector)  throws Exception {
+    }
+}
